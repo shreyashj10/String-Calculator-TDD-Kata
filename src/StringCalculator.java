fa302e087d19bbf819b90d583d280df73f20a6e9
unknown <shreyashj10@gmail.com>
2022-06-01 16:53:47 +0530

    Added test case for one number input and renamed previous test case name

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,11 @@
 
 public class StringCalculator {
 	public static int add(String test){
-		  return 0;
+		  if(test.equals("")) {
+			  return 0;
+		  }
+		  else {
+			  return Integer.parseInt(test);
+		  }
 		}
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 class StringCalculatorTest {
 	StringCalculator sCalculator;
 	@Test
-	void testAdd() {
+	void testAddForEmptyString() {
 		Assert.assertEquals(0, sCalculator.add(""));
+	}
+	
+	@Test
+	void testAddForOneNumber() {
+		Assert.assertEquals(1, sCalculator.add("1"));
 	}
 
 }
